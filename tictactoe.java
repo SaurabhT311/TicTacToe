a4497fd65520b7674df171ab150ab9afb42d52b6
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-12-26 23:25:06 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -1,1 +1,47 @@
-welcome
+import java.util.*;
+import java.util.Scanner;
+class tictactoe
+{
+	public static void uc2()
+	{
+		Scanner sc=new Scanner(System.in);
+		 System.out.println("Chhose between X and O: ");
+	        String input=sc.nextLine();
+		if(input.equals("X"))
+		{
+			System.out.println("It is Player turn");
+		}
+		else {
+			System.out.println("O has been chosen for computer");
+		}
+
+	}
+	public static void uc1()
+	{
+		char board[]=new char[10];
+		for(int i=1;i<10;i++)
+		{
+			System.out.println(board[i]+ " ");
+			board[i]='\0';
+		}
+	
+		System.out.println("|---|---|---|");
+		System.out.println("|---|---|---|");
+		System.out.println("|---|---|---|");
+		System.out.println("|---|---|---|");
+		System.out.println("|---|---|---|");
+		System.out.println();
+	}
+
+
+public static void main(String[] args)
+{
+	tictactoe ob=new tictactoe();
+	//Scanner sc=new Scanner(System.in);
+	//String input=sc.nextLine();
+	ob.uc1();
+	ob.uc2();
+}
+}
+
+
